65ad150be50e277f61d7e600a9dadcd9044ab13a
Ryne Stajcar <rynostajcar@gmail.com>
2018-03-12 19:59:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,10 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  attr_accessor :name, :grade
+  attr_accessor :name, :grade, :id
 
-  def attributes(name:, grade:)
-    @name = name
-    @grade = grade
+  def initialize(id=nil, name, grade)
+    
   end
 
 end
