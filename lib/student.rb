e2f8a7c81be8dbda359f4aeab3228699fd3d820a
Ryne Stajcar <rynostajcar@gmail.com>
2018-03-12 20:08:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,8 +4,17 @@ class Student
 
   attr_accessor :name, :grade, :id
 
-  def initialize(id=nil, name, grade)
-    
+  def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @grade = grade
   end
+
+  def self.create_table
+
+  def attributes
+    student = Student.new(name, grade)
+  end
+
 
 end
